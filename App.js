b445660eacdc8d13abc859696466497897a55189
akindoju <davidd.akindoju@gmail.com>
2022-02-02 09:50:32 +0100

    Created components and added scrollview

--- App.js
+++ App.js
@@ -1,24 +1,78 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from "react-native";
 export default function App() {
-  const [text, setText] = useState("Hello there");
+  const [text, setText] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  const textInputHandler = (enteredText) => {
+    setText(enteredText);
+  };
+
+  const buttonClickHandler = () => {
+    console.log(text);
+  };
+
+  const setGoalHandler = () => {
+    const newArr = [...goals];
+    newArr.push(text);
+    setGoals(newArr);
+  };
 
   return (
-    <View style={styles.container}>
-      <Text>{text}</Text>
-      <Button onPress={() => setText("Hello Mate")} title="Click Me" />
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.screen}>
+      <View style={styles.form}>
+        <TextInput
+          placeholder="Enter Goal"
+          style={styles.input}
+          value={text}
+          onChangeText={textInputHandler}
+        />
+        <Button title="Add" onPress={setGoalHandler} />
+      </View>
+      <View>
+        {goals.map((goal, idx) => (
+          <View key={idx} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
+  screen: {
+    paddingTop: 50,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+
+  form: {
+    flexDirection: "row",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-between",
+  },
+
+  input: {
+    width: "80%",
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    padding: 10,
+  },
+
+  listItem: {
+    padding: 10,
+    marginTop: 5,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
   },
 });
